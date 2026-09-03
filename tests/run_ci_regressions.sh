1dc26e2744ca4ecac44d96bb075b79745603e200
Carlos Velasco <xtreemze@gmail.com>
2026-09-03 21:46:04 +0800

    Regress changed firmware format scope

--- tests/run_ci_regressions.sh
+++ tests/run_ci_regressions.sh
@@ -10,6 +10,7 @@ run() {
 }
 
 run bash tests/qmk_patch_series.sh
+run bash tests/changed_firmware_format_scope.sh
 run bash tests/halcyon_tft_backlight_rules.sh
 run python3 tests/halcyon_module_sync.py
 run bash tests/halcyon_tft_layout_rules.sh
